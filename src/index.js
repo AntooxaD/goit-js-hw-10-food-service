86c1842907bc190a5292e55a95e5b2b8dee50b4c
Anton <82974569+AntooxaD@users.noreply.github.com>
2021-10-12 11:54:15 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+import menuElement from './menu.json';
+import menuTemplate from './template/template.hbs';
+import './refs.js';
+
+
+
+
+const menuRef = document.querySelector('.js-menu');
+
+menuRef.insertAdjacentHTML('beforeend', menuTemplate(menuElement));
